788f3f83799adbd09153f9726582972672df19f8
testrepo <111039355+chaithanyahanu@users.noreply.github.com>
2024-09-12 19:59:37 +0530

    Update main.tf

--- Terraform-Main/azurerm_translator/main.tf
+++ Terraform-Main/azurerm_translator/main.tf
@@ -7,15 +7,35 @@ data "azurerm_resource_group" "existing_rg" {
   name = "classplus-prod-RG"
 }
 
+# Use existing virtual network
+data "azurerm_virtual_network" "existing_vnet" {
+  name                = Prod-vnet
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+}
+
+# Use existing subnet
+data "azurerm_subnet" "existing_subnet" {
+  name                 = subnet_1
+  virtual_network_name = data.azurerm_virtual_network.existing_vnet.name
+  resource_group_name  = data.azurerm_resource_group.existing_rg.name
+}
+
 # Translator Cognitive Service
 resource "azurerm_cognitive_account" "translator" {
   name                = var.translator_name
   location            = data.azurerm_resource_group.existing_rg.location
   resource_group_name = data.azurerm_resource_group.existing_rg.name
-  kind                = "TextTranslation"  # Corrected kind
+  kind                = "TextTranslation"
   sku_name            = var.sku_name
 
   identity {
     type = "SystemAssigned"
+  }
+
+  network_acls {
+    default_action = "Deny"
+    virtual_network_subnets_ids = [
+      data.azurerm_subnet.existing_subnet.id
+    ]
   }
 }
