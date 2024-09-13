ec51666388ff4a54cc318e6c93dec72c32bb2626
testrepo <111039355+chaithanyahanu@users.noreply.github.com>
2024-09-13 12:39:34 +0530

    Update main.tf

--- Terraform-Main/azurerm_translator/main.tf
+++ Terraform-Main/azurerm_translator/main.tf
@@ -42,3 +42,37 @@ resource "azurerm_cognitive_account" "translator" {
     ignore_missing_vnet_service_endpoint = false
   }
 }
+
+resource "azurerm_private_dns_zone" "dns_zone" {
+  name                = "privatelink.cognitiveservices.azure.com"
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "dns_zone_link" {
+  name                  = "classplus-dns"
+  resource_group_name   = data.azurerm_resource_group.existing_rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.dns_zone.name
+  virtual_network_id    = data.azurerm_virtual_network.existing_vnet.id
+}
+
+resource "azurerm_private_endpoint" "cognitive_private_endpoint" {
+  name                = "cognitive-private-endpoint"
+  location            = data.azurerm_resource_group.existing_rg.location
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+  subnet_id           = data.azurerm_subnet.existing_subnet.id
+
+  private_service_connection {
+    name                           = "classplus-connection"
+    private_connection_resource_id = azurerm_cognitive_account.translator.id
+    subresource_names              = ["cognitiveservices"]
+    is_manual_connection           = false
+  }
+}
+
+resource "azurerm_private_dns_a_record" "dns_record" {
+  name                = azurerm_cognitive_account.translator.name
+  zone_name           = azurerm_private_dns_zone.dns_zone.name
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+  ttl                 = 300
+  records             = [azurerm_private_endpoint.cognitive_private_endpoint.private_ip_address]
+}
