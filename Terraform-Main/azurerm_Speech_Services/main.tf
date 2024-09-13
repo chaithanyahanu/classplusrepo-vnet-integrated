e93a0d86a921e4c38aef118b84fb29450599cf64
testrepo <111039355+chaithanyahanu@users.noreply.github.com>
2024-09-13 16:56:07 +0530

    Create main.tf

---
+++ Terraform-Main/azurerm_Speech_Services/main.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+# Use existing resource group
+data "azurerm_resource_group" "existing_rg" {
+  name = "classplus-prod-RG"
+}
+
+# Use existing virtual network
+data "azurerm_virtual_network" "existing_vnet" {
+  name                = "Prod-vnet"
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+}
+
+# Use existing subnet
+data "azurerm_subnet" "existing_subnet" {
+  name                 = "subnet_2"
+  virtual_network_name = data.azurerm_virtual_network.existing_vnet.name
+  resource_group_name  = data.azurerm_resource_group.existing_rg.name
+}
+# Translator Cognitive Service
+resource "azurerm_cognitive_account" "translator" {
+  name                = var.translator_name
+  location            = data.azurerm_resource_group.existing_rg.location
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+  kind                = "SpeechServices"
+  sku_name            = var.sku_name
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_acls {
+    default_action = "Deny"
+
+    virtual_network_rules {
+      subnet_id = data.azurerm_subnet.existing_subnet.id
+    }
+  }
+
+  custom_subdomain_name         = "classplusspeechservice-subdomain"
+}
