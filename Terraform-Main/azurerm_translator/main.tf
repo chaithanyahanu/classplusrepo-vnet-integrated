87dca3c4830c51365ce9d60076a1b3e47a2b6da6
testrepo <111039355+chaithanyahanu@users.noreply.github.com>
2024-09-13 12:57:58 +0530

    Update main.tf

--- Terraform-Main/azurerm_translator/main.tf
+++ Terraform-Main/azurerm_translator/main.tf
@@ -18,7 +18,6 @@ data "azurerm_subnet" "existing_subnet" {
   name                 = subnet_1
   virtual_network_name = data.azurerm_virtual_network.existing_vnet.name
   resource_group_name  = data.azurerm_resource_group.existing_rg.name
-}
 
 # Translator Cognitive Service
 resource "azurerm_cognitive_account" "translator" {
@@ -30,49 +29,19 @@ resource "azurerm_cognitive_account" "translator" {
 
   identity {
     type = "SystemAssigned"
-  }
+   }
 
   network_acls {
-    default_action             = "Deny"  # Deny all by default
-    virtual_network_subnet_ids = [data.azurerm_subnet.existing_subnet.id]  # Allow access from the existing subnet
+    default_action = "Deny"
+
+    ip_rules = [
+      "14.143.179.194",
+    ]
+
+    virtual_network_rules {
+      id = azurerm_virtual_network.existing_subnet.id
+    }
   }
 
-  virtual_network_rules {
-    subnet_id                          = data.azurerm_subnet.existing_subnet.id
-    ignore_missing_vnet_service_endpoint = false
-  }
-}
-
-resource "azurerm_private_dns_zone" "dns_zone" {
-  name                = "privatelink.cognitiveservices.azure.com"
-  resource_group_name = data.azurerm_resource_group.existing_rg.name
-}
-
-resource "azurerm_private_dns_zone_virtual_network_link" "dns_zone_link" {
-  name                  = "classplus-dns"
-  resource_group_name   = data.azurerm_resource_group.existing_rg.name
-  private_dns_zone_name = azurerm_private_dns_zone.dns_zone.name
-  virtual_network_id    = data.azurerm_virtual_network.existing_vnet.id
-}
-
-resource "azurerm_private_endpoint" "cognitive_private_endpoint" {
-  name                = "cognitive-private-endpoint"
-  location            = data.azurerm_resource_group.existing_rg.location
-  resource_group_name = data.azurerm_resource_group.existing_rg.name
-  subnet_id           = data.azurerm_subnet.existing_subnet.id
-
-  private_service_connection {
-    name                           = "classplus-connection"
-    private_connection_resource_id = azurerm_cognitive_account.translator.id
-    subresource_names              = ["cognitiveservices"]
-    is_manual_connection           = false
-  }
-}
-
-resource "azurerm_private_dns_a_record" "dns_record" {
-  name                = azurerm_cognitive_account.translator.name
-  zone_name           = azurerm_private_dns_zone.dns_zone.name
-  resource_group_name = data.azurerm_resource_group.existing_rg.name
-  ttl                 = 300
-  records             = [azurerm_private_endpoint.cognitive_private_endpoint.private_ip_address]
+  custom_subdomain_name = "custclsplusdomain"
 }
