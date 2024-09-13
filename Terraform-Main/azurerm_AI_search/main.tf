07613ff04cf6de90f58f6294e4158b22df9e8b85
testrepo <111039355+chaithanyahanu@users.noreply.github.com>
2024-09-13 16:43:56 +0530

    Update main.tf

--- Terraform-Main/azurerm_AI_search/main.tf
+++ Terraform-Main/azurerm_AI_search/main.tf
@@ -1,10 +1,24 @@
 provider "azurerm" {
   features {}
 }
-# Using an existing resource group
-data "azurerm_resource_group" "existing" {
+# Data block to reference the existing resource group
+data "azurerm_resource_group" "existing_rg" {
   name = "classplus-prod-RG"
 }
+
+# Use existing virtual network
+data "azurerm_virtual_network" "existing_vnet" {
+  name                = "Prod-vnet"
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+}
+
+# Use existing subnet
+data "azurerm_subnet" "existing_subnet" {
+  name                 = "subnet_1"
+  virtual_network_name = data.azurerm_virtual_network.existing_vnet.name
+  resource_group_name  = data.azurerm_resource_group.existing_rg.name
+}
+
 # Define Azure Cognitive Search Service
 resource "azurerm_search_service" "example" {
   name                = var.search_service_name
@@ -13,4 +27,14 @@ resource "azurerm_search_service" "example" {
   sku                 = "standard"
   replica_count   = var.replica_count
   partition_count = var.partition_count
+
+  network_acls {
+    default_action = "Deny"
+
+    virtual_network_rules {
+      subnet_id = data.azurerm_subnet.existing_subnet.id
+    }
+  }
+
+  custom_subdomain_name         = "classplsaisearch-subdomain"
 }
