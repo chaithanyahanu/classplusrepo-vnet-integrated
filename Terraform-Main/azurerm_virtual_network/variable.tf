1faba39b75fa86e90947375fc4a530615d82c98a
testrepo <111039355+chaithanyahanu@users.noreply.github.com>
2024-09-13 11:56:35 +0530

    Update variable.tf

--- Terraform-Main/azurerm_virtual_network/variable.tf
+++ Terraform-Main/azurerm_virtual_network/variable.tf
@@ -1,7 +1,7 @@
 variable "resourcegroup_name" {
   type        = string
   description = "The name of the resource group"
-  default     = "classplus-prod-RG-new"
+  default     = "classplus-prod-RG"
 }
 
 variable "location" {
