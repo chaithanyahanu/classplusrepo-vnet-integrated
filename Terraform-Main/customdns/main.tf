0175e7f2d0a2f2aa86f8a0faa032763c6c30fb6a
testrepo <111039355+chaithanyahanu@users.noreply.github.com>
2024-09-13 12:25:12 +0530

    Create main.tf

---
+++ Terraform-Main/customdns/main.tf
@@ -0,0 +1,55 @@
+provider "azurerm" {
+  features {}
+}
+
+# Use existing resource group
+data "azurerm_resource_group" "existing_rg" {
+  name = "classplus-prod-RG"
+}
+
+# Use existing virtual network
+data "azurerm_virtual_network" "existing_vnet" {
+  name                = Prod-vnet
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+}
+
+# Use existing subnet
+data "azurerm_subnet" "existing_subnet" {
+  name                 = subnet_1
+  virtual_network_name = data.azurerm_virtual_network.existing_vnet.name
+  resource_group_name  = data.azurerm_resource_group.existing_rg.name
+}
+
+resource "azurerm_private_dns_zone" "dns_zone" {
+  name                = "privatelink.cognitiveservices.azure.com"
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "dns_zone_link" {
+  name                  = "classplus-dns"
+  resource_group_name   = data.azurerm_resource_group.existing_rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.dns_zone.name
+  virtual_network_id    = data.azurerm_virtual_network.existing_vnet.id
+}
+
+resource "azurerm_private_endpoint" "cognitive_private_endpoint" {
+  name                = "cognitive-private-endpoint"
+  location            = data.azurerm_resource_group.existing_rg.location
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+  subnet_id           = data.azurerm_subnet.existing_subnet.id
+
+  private_service_connection {
+    name                           = "classplus-connection"
+    private_connection_resource_id = azurerm_cognitive_account.translator.id
+    subresource_names              = ["cognitiveservices"]
+    is_manual_connection           = false
+  }
+}
+
+resource "azurerm_private_dns_a_record" "dns_record" {
+  name                = azurerm_cognitive_account.translator.name
+  zone_name           = azurerm_private_dns_zone.dns_zone.name
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+  ttl                 = 300
+  records             = [azurerm_private_endpoint.cognitive_private_endpoint.private_ip_address]
+}
