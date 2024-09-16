d3a8db29807a02943dc0697800112303119e95fc
testrepo <111039355+chaithanyahanu@users.noreply.github.com>
2024-09-16 12:07:35 +0530

    Update variable.tf

--- Terraform-Main/azurerm_translator/variable.tf
+++ Terraform-Main/azurerm_translator/variable.tf
@@ -1,12 +1,12 @@
 variable "translator_name" {
   description = "The name of the Translator cognitive service"
   type        = string
-  default     = "clstranslator789"
+  default     = ""
 }
 
 variable "sku_name" {
   description = "The SKU of the Translator cognitive service (e.g. F0 for free or S1 for standard)"
   type        = string
-  default     = "S1"
+  default     = ""
 }
 
