b0dfc145e1867f91e9d87df1a51bce15d32b8c95
testrepo <111039355+chaithanyahanu@users.noreply.github.com>
2024-09-16 12:08:58 +0530

    Update variable.tf

--- Terraform-Main/azurerm_storage_account/variable.tf
+++ Terraform-Main/azurerm_storage_account/variable.tf
@@ -3,26 +3,26 @@
 variable "storage_account_name" {
   description = "The name of the Storage Account"
   type        = string
-  default     = "straccunt4578"
+  default     = ""
 }
 
 # Storage account tier: Standard or Premium
 variable "account_tier" {
   description = "The performance tier of the storage account"
   type        = string
-  default     = "Standard"
+  default     = ""
 }
 
 # Replication type: LRS, GRS, RAGRS, ZRS
 variable "account_replication_type" {
   description = "The replication type of the storage account"
   type        = string
-  default     = "LRS"
+  default     = ""
 }
 # Access tier: Hot or Cool
 variable "access_tier" {
   description = "Defines the access tier for the storage account"
   type        = string
-  default     = "Hot"
+  default     = ""
 }
 
