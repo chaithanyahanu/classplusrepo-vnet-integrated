3961348e5972ceb0947ca826128ec627d7fe4d5a
testrepo <111039355+chaithanyahanu@users.noreply.github.com>
2024-09-16 12:41:49 +0530

    Update translator.tf

--- Terraform-Main/module/translator.tf
+++ Terraform-Main/module/translator.tf
@@ -1,5 +1,5 @@
 /*module "translator" {
-source                   = "https://github.com/chaithanyahanu/classplusrepo-vnet-integrated/tree/main/Terraform-Main/azurerm_translator"
+source                   = "github.com/chaithanyahanu/classplusrepo-vnet-integrated/tree/main/Terraform-Main/azurerm_translator/"
 translator_name = "clstranslator789"
 sku_name = "S1"
 }*/
