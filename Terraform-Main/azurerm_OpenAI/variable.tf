87b310b71dc89fd376639b8c5ea7c6f9c79d52b4
testrepo <111039355+chaithanyahanu@users.noreply.github.com>
2024-09-16 12:12:15 +0530

    Update variable.tf

--- Terraform-Main/azurerm_OpenAI/variable.tf
+++ Terraform-Main/azurerm_OpenAI/variable.tf
@@ -1,11 +1,11 @@
 variable "openai_account_name" {
   description = "Name of the Azure OpenAI account."
   type        = string
-  default     = "clsopenai789"
+  default     = ""
 }
 
 variable "sku_name" {
   description = "The SKU name of the OpenAI account (e.g., S0)."
   type        = string
-  default     = "S0"
+  default     = ""
 }
