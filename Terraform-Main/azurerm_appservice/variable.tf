2ea3bbf22d3c016e1d8e4a528fa70dfff92e702b
testrepo <111039355+chaithanyahanu@users.noreply.github.com>
2024-09-16 12:10:38 +0530

    Update variable.tf

--- Terraform-Main/azurerm_appservice/variable.tf
+++ Terraform-Main/azurerm_appservice/variable.tf
@@ -2,11 +2,11 @@
 variable "app_service_plan_name" {
   type        = string
   description = "The name of the App Service Plan."
-  default     = "webappplan1"
+  default     = ""
 }
 
 variable "app_service_name" {
   type        = string
   description = "The name of the App Service."
-  default     = "webapp987"
+  default     = ""
 }
